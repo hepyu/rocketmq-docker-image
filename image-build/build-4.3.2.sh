2ea07cbcf672da0ed233a90886a4dd44e28fde5b
hepengyuan <hepengyuan@coohua.com>
2019-09-25 11:02:38 +0800

    update

---
+++ image-build/build-4.3.2.sh
@@ -0,0 +1,8 @@
+rm -rf rocketmq/conf/*
+cp config/* rocketmq/conf/
+
+rm -rf rocketmq/bin/runserver.sh
+rm -rf rocketmq/bin/runbroker.sh
+cp scripts/* rocketmq/bin/
+
+sh build-image.sh 4.3.2 centos
